6da9da85da977edfd5620c09e98c2fce5e798781
Tulika Das <tulikadas1029@gmail.com>
2022-01-22 18:59:46 +0530

    client section icon section and story to share section added

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -11,3 +11,30 @@ if (document.querySelector('.orange-second-hero-section')) {
         autoplayButtonOutput: false,
     })
 }
+
+
+if (document.querySelector('.client-section')) {
+    tns({
+        container: '.client-section .slides',
+        items: 1,
+        nav: true,
+        autoplay: true,
+        controls:false,
+        autoplayTimeout:3000,
+        autoplayButtonOutput: false,
+    })
+}
+
+if (document.querySelector('.story-share-section')) {
+    tns({
+        container: '.story-share-section .slides',
+        items: 3,
+        nav: false,
+        gutter:25,
+        autoplay: true,
+        controls:true,
+        autoplayTimeout:3000,
+        autoplayButtonOutput: false,
+        controlsText:['<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>']
+    })
+}
